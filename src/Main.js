490433fcf80c7baf94219fde42780ee73bf2c332
Naatty123 <nattynengeda@gmail.com>
2022-07-02 21:43:11 +0300

    Card Completed

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import Card from './Card'
 
 export default function Main() {
   return (
-    <div className='min-h-screen'>
-       
+    <div className='min-h-screen grid grid-cols-1 md:grid-cols-10 mt-20'>
+        <aside className='col-span-2 bg-orange-300 hidden md:block'>
+        <ul className='w-full pt-5'>
+            <li className='w-full p-4 bg-orange-400 cursor-pointer hover:bg-orange-500'>
+                <a>Blogs</a>
+            </li>
+            <li className='w-full p-4 bg-orange-400 mt-1 cursor-pointer hover:bg-orange-500'>
+                <a>Add Blog</a>
+            </li>
+        </ul>
+        </aside>
+        <div className='col-span-8 bg-violet-600 flex flex-col justify-center items-center md:items-start px-5 md:grid  md:grid-cols-3 gap-5 pt-5'>
+            <Card  head="First Head" content="This is the Most disgusting Content i Ever Read Soo just leave me alone"/>
+            <Card  head="First Head" content="This is the Most disgusting Content i Ever Read Soo just leave me alone"/>
+            <Card  head="First Head" content="This is the Most disgusting Content i Ever Read Soo just leave me alone"/>
 
+        </div>
 
 
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,46 @@ import Footer from './Footer';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+function fullName(mnae){
+  return mname.firstName + ' ' + mname.lastName
+}
+const mname ={
+  firstName: 'Melat',
+  lastName: 'Engeda',
+}
+const fullElement = (
+  <div>
+    <h1>Name: Natnael Engead</h1>
+    <h2>Upload: Not Uploaded</h2>
+    <p>Other: No Other Things to List</p>
+  </div>
+)
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element)
+
+}
+
+
+
+// const Name = <h1 className='text-red-400 text-4xl'>{fullName(mname)}</h1>
 root.render(
   <React.StrictMode>
     <Header />
     <Main />
     <Footer />
   </React.StrictMode>
+//  fullElement
+  // React.createElement('h1',{className: 'text-5xl'}, 'Natty Marn')
+  // element
 );
+// setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
